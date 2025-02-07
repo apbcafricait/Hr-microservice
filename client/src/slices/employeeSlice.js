0be249a79e21a6ee6adb3bd62952bd77e9362dbc
samuel-254-ke <wanjugusamuel254@gmail.com>
2025-02-07 15:29:49 +0300

    epmloyees apislices done

--- client/src/slices/employeeSlice.js
+++ client/src/slices/employeeSlice.js
@@ -1,86 +1,61 @@
-// src/redux/slices/employeeApiSlice.js
-import { apiSlice } from './apiSlice';
-import { EMPLOYEE_URL } from '../Constants/constants';
+import { EMPLOYEE_URL } from "../Constants/constants";
+import { apiSlice } from "./apiSlice";
 
 export const employeeApiSlice = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
-    // Fetch all employees with pagination, filtering, and searching
+    
+    // Fetch all employees
     getAllEmployees: builder.query({
       query: ({ page = 1, limit = 10, organisationId, search = '' }) => ({
         url: `${EMPLOYEE_URL}`,
-        method: 'GET',
-        params: {
-          page,
-          limit,
-          organisationId,
-          search,
-        },
+        method: "GET",
+        params: { page, limit, organisationId, search },
       }),
-      providesTags: ['Employee'],
-      transformResponse: (response) => {
-        return response.data;
-      },
     }),
 
     // Fetch a single employee by ID
     getEmployee: builder.query({
-      query: (id) => ({
-        url: `${EMPLOYEE_URL}/${id}`,
-        method: 'GET',
-      }),
-      providesTags: ['Employee'],
+      query: (id) => `${EMPLOYEE_URL}/${id}`,
     }),
 
     // Create a new employee
     createEmployee: builder.mutation({
-      query: (newEmployee) => ({
+      query: (body) => ({
         url: `${EMPLOYEE_URL}`,
-        method: 'POST',
-        body: newEmployee,
+        method: "POST",
+        body,
       }),
-      invalidatesTags: ['Employee'],
     }),
 
     // Update an existing employee
     updateEmployee: builder.mutation({
-      query: ({ id, updateData }) => ({
+      query: ({ id, body }) => ({
         url: `${EMPLOYEE_URL}/${id}`,
-        method: 'PUT',
-        body: updateData,
+        method: "PUT",
+        body,
       }),
-      invalidatesTags: ['Employee'],
     }),
 
     // Delete an employee
     deleteEmployee: builder.mutation({
       query: (id) => ({
         url: `${EMPLOYEE_URL}/${id}`,
-        method: 'DELETE',
+        method: "DELETE",
       }),
-      invalidatesTags: ['Employee'],
     }),
 
     // Get employees by organisation
     getEmployeesByOrganisation: builder.query({
-      query: (organisationId) => ({
-        url: `${EMPLOYEE_URL}/organisation/${organisationId}`,
-        method: 'GET',
-      }),
-      providesTags: ['Employee'],
+      query: (organisationId) => `${EMPLOYEE_URL}/organisation/${organisationId}`,
     }),
 
     // Get employee statistics
     getEmployeeStats: builder.query({
-      query: (organisationId) => ({
-        url: `${EMPLOYEE_URL}/stats/${organisationId}`,
-        method: 'GET',
-      }),
-      providesTags: ['Employee'],
+      query: (organisationId) => `${EMPLOYEE_URL}/stats/${organisationId}`,
     }),
   }),
 });
 
-// Export hooks for each endpoint
 export const {
   useGetAllEmployeesQuery,
   useGetEmployeeQuery,
