4d20b587b4419813866d4dbf3e93f22a79ad929d
samuel-254-ke <wanjugusamuel254@gmail.com>
2025-05-07 13:29:32 +0300

    admins only routes

--- backend/Routes/userRoute.js
+++ backend/Routes/userRoute.js
@@ -4,11 +4,9 @@ import { authenticated, admin, manager } from "../middleware/Authentication.js";
 
 const router = express.Router();
 
-router.get('/', getAllUsers)
-
+router.get('/', authenticated, admin(), getAllUsers);
 router.post("/", RegisterUser);
 router.post("/login", loginUser);
 router.post("/logout", logout)
-
 
 export default router;
